bad4d133838c1019d5c2cd640daffa50031d1eeb
Saul <emma.saul2002@gmail.com>
2026-03-09 11:43:26 +0100

    lavede en liste at teste med

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+
+    public static void main(){
+
+
+        Opskriftsbog[] opskrifter = new Opskriftsbog[5];
+            opskrifter[0] = new Opskriftsbog("Donut", "mælk, mel, sukker", "lav donuts", true);
+            opskrifter[1] = new Opskriftsbog("lassagne", "mælk, mel, sukker", "lav donuts", false);
+            opskrifter[2] = new Opskriftsbog("Donuts igen", "mælk, mel, sukker", "lav donuts", true);
+            opskrifter[3] = new Opskriftsbog("salat", "mælk, mel, sukker", "lav donuts", false);
+            opskrifter[4] = new Opskriftsbog("peber", "peber", "lav donuts", true);
+    }
+
+
+
 }
