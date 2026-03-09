fe7725f1698a9da00521e51777ceed978f932967
JeffRomance <maxi_johansen@hotmail.com>
2026-03-09 11:31:44 +0100

    add

--- src/main/java/Opskriftsbog.java
+++ src/main/java/Opskriftsbog.java
@@ -1,7 +1,20 @@
 public class Opskriftsbog {
-    String titel;
-    String[] ingredienser;
-    String fremgangsmåde;
+    String title;
+    String ingredients;
+    String procedure;
 
+    public Opskriftsbog(String title, String ingredients, String procedure) {
+        this.title = title;
+        this.ingredients = ingredients;
+        this.procedure = procedure;
 
+    }
+
+    public String toString() {
+        return String.format("""
+                Opskrift: %s
+                Ingredienser: %s
+                Fremgangsmåde: %s
+                """, title, ingredients, procedure);
+    }
 }
